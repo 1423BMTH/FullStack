480d9c7c53bc5448cec72c69cb4d697ce8ff0202
1423BMTH <mmyyproj@gmail.com>
2023-06-08 12:33:07 -0700

    destructuring
    extract values of one object directly

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,24 +1,28 @@
 const Hello = (props) => {
-
-  //Component helper function
-  const bornYr = () => new Date().getFullYear() - props.age;
+  //Destructuring: extracting values from objects or 
+  //arrays and assigning them to variables in a concise and convenient manner.
+  const name = props.name;
+  const age = props.age;
+  //End of destructuring
+  
+  const bornYr = () => new Date().getFullYear() - age;
   console.log("This is", bornYr());
   return (
     <div>
-      {bornYr()}
+      <p>THEIR name is {name} and age is {age}</p>
+      <p>THEY were born in {bornYr()}</p>
     </div>
   );
 }
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const name = 'SUN YING'
+  const age = 27
   console.log("Fuck that bitch")
-
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello age={26 + 10} />
+      <h1>THE FOLLOWING IS AN INBECILE!</h1>
+      <Hello name = {name} age={age} />
     </div>
   )
 }
