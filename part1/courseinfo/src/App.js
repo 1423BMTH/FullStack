8198c92ba1630a339ff9f776910e91486121fd90
1423BMTH <mmyyproj@gmail.com>
2023-06-08 00:04:46 -0700

    exe1.5_Only_One_Array
    Let's take the changes one step further. Change the course and its parts into a single JavaScript object. Fix everything that breaks.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,28 +1,29 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
-    /*However, do not pass different objects as separate props from
-     the App component to the components Content and Total. 
-     Instead, pass them directly as an array: */
+    /*Change the course and its parts into a single JavaScript object. 
+    Fix everything that breaks. */
     <div>
-      <Header course={course} />
-      <Content arrayObj ={parts} />
-      <Total arrayObj={parts} />
+      
+      <Content arrayObj ={course} />
+      
     </div>    
   )
 }
@@ -32,29 +33,31 @@ const Content = (props) => {
   const {arrayObj} = props;
   return (
     <div>
-      <p>{arrayObj[0].name} {arrayObj[0].exercises}</p>
-      <p>{arrayObj[1].name} {arrayObj[1].exercises}</p>
-      <p>{arrayObj[2].name} {arrayObj[2].exercises}</p>
+      <h1>{arrayObj.name}</h1>
+      <p>{arrayObj.parts[0].name} {arrayObj.parts[0].exercises}</p>
+      <p>{arrayObj.parts[1].name} {arrayObj.parts[1].exercises}</p>
+      <p>{arrayObj.parts[2].name} {arrayObj.parts[2].exercises}</p>
+      <p>Total is {arrayObj.parts[0].exercises + arrayObj.parts[1].exercises + arrayObj.parts[2].exercises}</p>
     </div>    
   )
 } 
 
-const Total = (props) => {
-  //Accessing multiple objects at the same time
-  // const {obj1, obj2, obj3} = props;
-  //return(<p>{obj1.exercise + obj2.exercise + obj3.exercise}</p>);
-  const {arrayObj} = props;
-  return (
-      <p>{arrayObj[0].exercises+ arrayObj[1].exercises + arrayObj[2].exercises}</p>
-  )
-}
+// const Total = (props) => {
+//   //Accessing multiple objects at the same time
+//   // const {obj1, obj2, obj3} = props;
+//   //return(<p>{obj1.exercise + obj2.exercise + obj3.exercise}</p>);
+//   const {arrayObj} = props;
+//   return (
+//       <p>{arrayObj[0].exercises+ arrayObj[1].exercises + arrayObj[2].exercises}</p>
+//   )
+// }
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
+// const Header = (props) => {
+//   return (
+//     <div>
+//       <h1>{props.course}</h1>
+//     </div>
+//   )
+// }
 
 export default App
