25cfead2488b1712f423eee2a6a7d2c4cb689bac
1423BMTH <mmyyproj@gmail.com>
2023-06-10 16:13:03 -0700

    exe_1.6_useState
    Click event handler

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,19 +1,31 @@
 import { useState } from 'react'
-const App = () => {
-  const [ counter, setCounter ] = useState(0);
 
-  const handleClick = () => {
-    console.log('clicked');
-    setCounter(counter + 1);
-  }
+const App = () => {
+  // save clicks of each button to its own state
+  //The first element is the current state value,
+  // and the second element is the updater function associated with that state variable.
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodHandler = () => setGood(good + 1);
+  const neutralHandler = () => setNeutral(neutral + 1);
+  const badHandler = () => setBad(bad + 1);
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={handleClick}>
-        plus
-      </button>
+      <h1>give feedback</h1>
+      <p>
+      <button onClick={goodHandler}>good</button>
+      <button onClick = {neutralHandler}>neutral</button>
+      <button onClick = {badHandler}>bad</button>
+      <h1>statics</h1>
+      </p>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
-export default App;
+
+export default App
