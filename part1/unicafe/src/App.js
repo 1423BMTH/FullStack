afb79961268bde378a288acffc02ada3ee3b970b
1423BMTH <mmyyproj@gmail.com>
2023-06-11 19:48:03 -0700

    exe1.4 display statistics only once feedback has been gathered

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -14,17 +14,26 @@ import { useState } from 'react'
 //   )
 // }
 const Statistics = ({good, neutral,bad}) => {
-  return (
-    <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good * 1 + neutral *0 + bad * (-1))/(good + bad + neutral)}</p>
-      <p>positive {good/(good + bad + neutral)*100} %</p>
-    </div>
-  )
-}
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    );
+  }
+  else{
+    return (
+      <div>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {good + neutral + bad}</p>
+        <p>average {(good * 1 + neutral *0 + bad * (-1))/(good + bad + neutral)}</p>
+        <p>positive {good/(good + bad + neutral)*100} %</p>
+      </div>
+    );
+  };
+};
 
 const App = () => {
   // save clicks of each button to its own state
@@ -55,7 +64,7 @@ const App = () => {
       </p>
       <Statistics good = {good} bad = {bad} neutral = {neutral}/>
     </div>
-  )
+  );
 }
 
 export default App;
